d022a805c041247929ec2f9c2055479bbdc98ea7
Syedrizwan23 <syedrizwanghani@gmail.com>
2025-01-01 18:46:37 +0500

    WhyChooseUs

---
+++ src/components/WhyChooseUs.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React from "react";
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+
+const musicSchoolContent = [
+    {
+      title: 'Discover Your Sound with Us: A Personal Journey in Music Mastery',
+      description:
+        'Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.',
+        content: (
+            <div className="h-full w-full bg-gradient-to-br from-cyan-500 to-emerald-500 flex items-center justify-center text-white">
+              
+            </div>
+          ),
+    },
+    {
+      title: 'Discover Your Sound with Us: A Personal Journey in Music Mastery',
+      description:
+        'Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.',
+        content: (
+            <div className="h-full w-full bg-gradient-to-br from-orange-500 to-yellow-500 flex items-center justify-center text-white">
+              
+            </div>
+          ),
+    },
+    {
+      title: 'Discover Your Sound with Us: A Personal Journey in Music Mastery',
+      description:
+        'Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.',
+        content: (
+            <div className="h-full w-full bg-gradient-to-br from-pink-500 to-emerald-500 flex items-center justify-center text-white">
+              
+            </div>
+          ),
+    },
+    {
+      title: 'Live Feedback & Engagement',
+      description:
+        'Immerse yourself in an interactive learning experience where feedback is immediate, just like real-time changes in a collaborative project. This approach enhances your understanding and mastery of music concepts and performance techniques.',
+        content: (
+            <div className="h-full w-full bg-gradient-to-br from-red-500 to-pink-500 flex items-center justify-center text-white">
+              
+            </div>
+          ),
+    },
+    {
+      title: 'Cutting-Edge Curriculum',
+      description:
+        'Our curriculum is continuously updated to include the latest music education trends and technologies, ensuring you’re always learning with the most current and effective methods. Say goodbye to outdated materials and welcome an education that evolves with the industry.',
+        content: (
+            <div className="h-full w-full bg-gradient-to-br from-cyan-500 to-emerald-500 flex items-center justify-center text-white">
+              
+            </div>
+          ),
+    },
+    {
+      title: 'Limitless Learning Opportunities',
+      description:
+        'With our expansive resource library and dynamic course offerings, you’ll never find yourself without something new to explore. Our platform provides continuous opportunities for growth, ensuring your musical skills are always advancing.',
+        content: (
+            <div className="h-full w-full bg-gradient-to-br from-cyan-500 to-emerald-500 flex items-center justify-center text-white">
+              
+            </div>
+          ),
+    },
+  ];
+
+function WhyChooseUs() {
+  return (
+    <div className="p-10">
+        <StickyScroll content={musicSchoolContent} />
+    </div>
+  )
+}
+
+export default WhyChooseUs
