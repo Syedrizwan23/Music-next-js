86572a2f9a0a4bffab8a7a9d236c3208c8477ea3
Syedrizwan23 <syedrizwanghani@gmail.com>
2025-01-01 15:45:39 +0500

    hero section cmpltd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,24 @@
 import type { Config } from "tailwindcss";
 
+// plugin to add each Tailwind color as a global CSS
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function addVariablesForColors({addBase, theme}: any) {
+  const allColors = flattenColorPalette(theme('colors'));
+  
+  if (typeof allColors === 'object' && allColors !== null) {
+    const newVars = Object.fromEntries(
+      Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+    );
+    
+    addBase({
+      ':root': newVars,
+    });
+  } else {
+    console.warn('allColors is not an object or is null');
+  }
+}
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +28,31 @@ export default {
   darkMode:'class',
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
+function flattenColorPalette(_arg0: Record<string, string | Record<string, string>>) {
+  // Implement the function logic here
+  return _arg0;
+}
+
