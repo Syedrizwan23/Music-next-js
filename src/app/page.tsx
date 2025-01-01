cbfefccf55f698cba06419f5a15227d5d0621b85
Syedrizwan23 <syedrizwanghani@gmail.com>
2025-01-01 17:42:53 +0500

    course cmpltd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // import { main } from "framer-motion/client";
 
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/ui/HeroSection";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
+      <FeaturedCourses/>
     </main>
   );
 }
